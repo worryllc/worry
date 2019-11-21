acabd4cf6aa4efccd52d8b107c6db3d77c4c1e23
worryllc <worry.llc@gmail.com>
2019-11-20 21:25:16 -0500

    First commit

---
+++ worry.js
@@ -0,0 +1,21 @@
+'use strict';
+
+console.log('Hello world');
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function () { myFunction() };
+
+// Get the navbar
+var topnav = document.getElementById("topnav");
+
+// Get the offset position of the navbar
+var sticky = topbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+    if (window.pageYOffset >= sticky) {
+        topnav.classList.add("sticky")
+    } else {
+        topnav.classList.remove("sticky");
+    }
+}
